c4b34d91c762ab67f0be0501cad278762edad0d0
Davidmide <Davidmide07@gmail.com>
2024-09-06 06:48:55 +0100

    single page

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
 
 type CardProp = {
   image: string;
@@ -9,9 +10,13 @@ type CardProp = {
 
 const Card = ({ title, image, author, category }: CardProp) => {
   return (
-    <div className="control bg-green-300 h-[70%] mt-4 md:mt-0">
+    <div className="control bg-gray-50 p-2 rounded-xl h-[70%] mt-4 md:mt-0">
       <div className="img bg-gray-200 rounded-xl w-auto h-[50%]">
-        <img src={image} alt="image" className="h-[20rem] md:h-full w-full rounded-xl" />
+        <img
+          src={image}
+          alt="image"
+          className="h-[20rem] md:h-full w-full rounded-xl"
+        />
       </div>
       <div className="text-img py-4">
         <h1>Title: {title}</h1>
@@ -19,9 +24,11 @@ const Card = ({ title, image, author, category }: CardProp) => {
         <p>Category: {category}</p>
       </div>
       <div className="btn bg-gray-400 rounded-md">
-        <button className="bg-indigo-600 text-white rounded-md p-2 w-full">
-          Check
-        </button>
+        <Link to={`/material/${title}`}>
+          <button className="bg-indigo-600 text-white rounded-md p-2 w-full">
+            Check
+          </button>
+        </Link>
       </div>
     </div>
   );
--- src/pages/material/singleMaterial/index.tsx
+++ src/pages/material/singleMaterial/index.tsx
@@ -1,10 +1,31 @@
 import { useParams } from "react-router-dom";
 import image from "../../../assets/eng.jfif";
+import { Link } from "react-router-dom";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
 
 const SingleMaterial = () => {
   const { single } = useParams();
   return (
     <div className="single font-serif">
+      {/* <div className="btn flex flex-col md:items-end">
+        <Link
+          to={"/material"}
+          className="text-white md:w-[20%] bg-pink-500"
+        >
+          <ArrowLeftIcon className="w-full h-[3.5rem] bg-indigo-600" />
+        </Link>
+      </div> */}
+      <div className="btn rounded-md flex flex-col md:items-center">
+        <Link
+          to={`/material`}
+          className="flex flex-col items-center bg-indigo-600 text-white rounded-md p-2 md:w-1/4"
+        >
+          <ArrowLeftIcon className="w-8 h-8" />
+          {/* <button className="bg-indigo-600 text-white rounded-md p-2">
+            Back
+          </button> */}
+        </Link>
+      </div>
       <h1>Name of book: {single}</h1>
       <div className="detail flex flex-col md:flex-row md:items-center md:justify-around">
         <div className="image md:w-[50%] md:h-[50%]">
